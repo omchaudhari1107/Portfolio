337dbc70c0e622b06f074a0399865f556338c86b
om <kmcamja@gmail.com>
2023-11-28 10:51:21 +0530

    commit

--- privateWeb/settings.py
+++ privateWeb/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR /  'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
     # ...
@@ -133,10 +133,9 @@ STORAGES = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 # settings.py
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'omchaudhari1107@gmail.com'
-EMAIL_HOST_PASSWORD = 'vxsoymjzgzzllajf'
-
+EMAIL_HOST_USER = "omchaudhari1107@gmail.com"
+EMAIL_HOST_PASSWORD = "vxsoymjzgzzllajf"
