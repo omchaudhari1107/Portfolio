fd6f83a7a3dd96e4c93c39fd2848c9269bb79ad0
om <kmcamja@gmail.com>
2023-11-28 10:47:59 +0530

    commit

--- privateWeb/settings.py
+++ privateWeb/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / 'satic']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR /  'staticfiles'
 
 STORAGES = {
