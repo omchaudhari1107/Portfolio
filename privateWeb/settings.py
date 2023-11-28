a46baa4dec0e212545382f443c9cfa28e17042e6
om <kmcamja@gmail.com>
2023-11-28 08:18:24 +0530

    final commit

--- privateWeb/settings.py
+++ privateWeb/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 # settings.py
